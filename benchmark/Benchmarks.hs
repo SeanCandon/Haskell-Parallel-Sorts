d409d4f42ae17f244734fff138d8f316bf63efcc
Sean Candon <candons@tcd.ie>
2019-10-07 15:46:43 +0100

    benchmarking fixed

--- benchmark/Benchmarks.hs
+++ benchmark/Benchmarks.hs
@@ -8,14 +8,15 @@ import Sorting
 
 main = run
 
-run :: IO Benchmark
+run :: IO ()
 run = do
         g <- getStdGen
         let input1 = (take 100 (randomRs (0,100) g))::[Integer]
-        return $ runBenches input1
+        defaultMain $ runBenches input1
 
-runBenches :: [Integer] -> Benchmark
-runBenches input1 = bgroup "Quicksort1"
-              [ bench "100"
-                ( whnf quicksort input1 )
-              ]
+runBenches :: [Integer] -> [Benchmark]
+runBenches input1 = [ bgroup "Quicksort1"
+                      [ bench "100"
+                        ( whnf quicksort input1 )
+                      ]
+                    ]
